cf6e98ebc346c7b42f4b882f7f7849fd1aa7b9e1
Robby <robbylebotha@gmail.com>
2021-06-10 23:13:04 +0200

    Prevented auto selecting item on loading spinner

--- app/src/main/java/it/ads/app/cityexplorerkt/MainActivity.kt
+++ app/src/main/java/it/ads/app/cityexplorerkt/MainActivity.kt
@@ -1,22 +1,26 @@
 package it.ads.app.cityexplorerkt
 
 import android.app.ProgressDialog
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.util.Log
 import android.view.View
 import android.widget.AdapterView
 import android.widget.ArrayAdapter
+import androidx.appcompat.app.AppCompatActivity
 import it.ads.app.city_explorer_sdk.CityExplore
 import it.ads.app.city_explorer_sdk.interfaces.CityExploreCallBack
 import it.ads.app.city_explorer_sdk.network.CheckNetwork
 import kotlinx.android.synthetic.main.activity_main.*
-import java.util.ArrayList
+import java.util.*
 
 class MainActivity : AppCompatActivity() {
     val cityEplorer = CityExplore(this)
     val TAG = "city_explorer_kotlin"
     var progressDialog: ProgressDialog? = null
+    //these swithces will prevent spinners from automatically selcting first option on load
+    var spinnerMallSwitch = 1
+    var spinnerCitySwitch = 1
+    var spinnerShopSwitch = 1
 
     override fun onCreate(savedInstanceState: Bundle?) {
 
@@ -47,6 +51,10 @@ class MainActivity : AppCompatActivity() {
 
                             override fun onItemSelected(parent: AdapterView<*>?, view: View?, position: Int, id: Long) {
 
+                                if (spinnerCitySwitch == 1) {
+                                    spinnerCitySwitch = 0
+                                    return
+                                }
                                 //When user selects city, fetch mall data and create spinner
                                 val cityName = parent!!.getItemAtPosition(position).toString()
                                 Log.i(TAG, cityName)
@@ -54,6 +62,10 @@ class MainActivity : AppCompatActivity() {
                                 cityEplorer.getMalls(cityName, object : CityExploreCallBack{
                                     override fun onSuccess(list: ArrayList<String>?) {
                                         Log.i(TAG, list!!.get(0))
+                                        spinnerMallSwitch = 1 //reset switches
+                                        spinnerShopSwitch = 1
+                                        shopSpinner.adapter = null //remove items in lists
+                                        mallSpinner.adapter = null
                                         createMallSpinner(list)
                                         progressDialog!!.hide()
                                     }
@@ -96,7 +108,10 @@ class MainActivity : AppCompatActivity() {
             }
 
             override fun onItemSelected(parent: AdapterView<*>?, view: View?, position: Int, id: Long) {
-
+                if (spinnerMallSwitch == 1) {
+                    spinnerMallSwitch = 0
+                    return
+                }
                 val mallName = parent!!.getItemAtPosition(position).toString()
                 Log.i("city", mallName)
                 progressDialog!!.show()
@@ -134,6 +149,10 @@ class MainActivity : AppCompatActivity() {
 
             override fun onItemSelected(parent: AdapterView<*>?, view: View?, position: Int, id: Long) {
 
+                if (spinnerShopSwitch == 1) {
+                    spinnerShopSwitch = 0
+                    return
+                }
                 val shopName = parent!!.getItemAtPosition(position).toString()
                 Log.i("city", shopName)
             }
