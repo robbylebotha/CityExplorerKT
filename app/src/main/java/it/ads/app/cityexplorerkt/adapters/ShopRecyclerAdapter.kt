877b2f5da50748a9cef100a96abb2bc04a9457c5
Robby <robbylebotha@gmail.com>
2021-06-13 17:29:55 +0200

    Added small animation to list.

--- app/src/main/java/it/ads/app/cityexplorerkt/adapters/ShopRecyclerAdapter.kt
+++ app/src/main/java/it/ads/app/cityexplorerkt/adapters/ShopRecyclerAdapter.kt
@@ -4,7 +4,7 @@ import android.content.Context
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import android.widget.Toast
+import android.view.animation.AnimationUtils
 import androidx.recyclerview.widget.RecyclerView
 import it.ads.app.cityexplorerkt.R
 import it.ads.app.cityexplorerkt.models.Shop
@@ -12,6 +12,7 @@ import it.ads.app.cityexplorerkt.viewmodels.ShopViewModel
 import kotlinx.android.synthetic.main.shop_row.view.*
 
 class ShopRecyclerAdapter(val viewModel: ShopViewModel, val arrayList: ArrayList<Shop>, val context: Context): RecyclerView.Adapter<ShopRecyclerAdapter.NotesViewHolder>() {
+    private var lastPosition = -1
     override fun onCreateViewHolder(
         parent: ViewGroup,
         viewType: Int
@@ -22,6 +23,13 @@ class ShopRecyclerAdapter(val viewModel: ShopViewModel, val arrayList: ArrayList
 
     override fun onBindViewHolder(holder: ShopRecyclerAdapter.NotesViewHolder, position: Int) {
         holder.bind(arrayList.get(position))
+        //some custom animation because lists are boring.
+        val animation = AnimationUtils.loadAnimation(
+            context,
+            if (position > lastPosition) R.anim.up_from_bottom else R.anim.down_from_top
+        )
+        holder.itemView.startAnimation(animation)
+        lastPosition = position
     }
 
     override fun getItemCount(): Int {
@@ -33,10 +41,7 @@ class ShopRecyclerAdapter(val viewModel: ShopViewModel, val arrayList: ArrayList
     inner  class NotesViewHolder(private val view: View) : RecyclerView.ViewHolder(view) {
         fun bind(shop: Shop){
             view.shop_name.text = shop.name
-//            binding.delete.setOnClickListener {
-//                viewModel.remove(shop)
-//                notifyItemRemoved(arrayList.indexOf(shop))
-//            }
+            view.show_website.text = shop.website?.toLowerCase()
         }
 
     }
